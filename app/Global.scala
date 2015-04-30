089965c4ad9656fb3751bfbfa060ff45046a7773
Irving Elyasib Santiago <elyasib.isc@gmail.com>
2015-04-30 00:19:17 -0500

    now content is gzipped

---
+++ app/Global.scala
@@ -0,0 +1,42 @@
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.filters.gzip.GzipFilter
+import scala.concurrent.Future
+
+object Global extends WithFilters(new GzipFilter) with GlobalSettings {
+	
+	//private def getSubdomain (request: RequestHeader) = request.domain.replaceFirst("[\\.]?[^\\.]+[\\.][^\\.]+$", "")
+	
+	//override def onRouteRequest (request: RequestHeader) = getSubdomain(request) match {
+	//	case "admin" => admin.Routes.routes.lift(request)
+	//	case _ => web.Routes.routes.lift(request)
+	//}
+	
+    override def onStart(app: Application) {
+      Logger.info("Application has started")
+    }
+	
+    override def onStop(app: Application) {
+      Logger.info("Application shutdown")
+    }
+
+	//// 404 - page not found error
+	//override def onHandlerNotFound (request: RequestHeader) = getSubdomain(request) match {
+	//	case "admin" => GlobalAdmin.onHandlerNotFound(request)
+	//	case _ => GlobalWeb.onHandlerNotFound(request)
+	//}
+	//
+	//// 500 - internal server error
+	//override def onError (request: RequestHeader, throwable: Throwable) = getSubdomain(request) match {
+	//	case "admin" => GlobalAdmin.onError(request, throwable)
+	//	case _ => GlobalWeb.onError(request, throwable)
+	//}
+	//
+	//// called when a route is found, but it was not possible to bind the request parameters
+	//override def onBadRequest (request: RequestHeader, error: String) = getSubdomain(request) match {
+	//	case "admin" => GlobalAdmin.onBadRequest(request, error)
+	//	case _ => GlobalWeb.onBadRequest(request, error)
+	//}
+
+}
--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq(
+  filters,
   jdbc,
   anorm,
   cache,
